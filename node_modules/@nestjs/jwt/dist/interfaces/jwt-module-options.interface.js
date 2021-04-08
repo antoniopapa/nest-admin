4883173c90531ec676e5799cc6b617145c022987
Antonio Papa <antoniopapa1991@gmail.com>
2021-04-08 11:58:55 +0200

    first commit

--- node_modules/@nestjs/jwt/dist/interfaces/jwt-module-options.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var JwtSecretRequestType;
-(function (JwtSecretRequestType) {
-    JwtSecretRequestType[JwtSecretRequestType["SIGN"] = 0] = "SIGN";
-    JwtSecretRequestType[JwtSecretRequestType["VERIFY"] = 1] = "VERIFY";
-})(JwtSecretRequestType = exports.JwtSecretRequestType || (exports.JwtSecretRequestType = {}));
--- node_modules/@nestjs/jwt/dist/jwt.constants.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_MODULE_OPTIONS = 'JWT_MODULE_OPTIONS';
